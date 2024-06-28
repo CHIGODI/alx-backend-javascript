04c8281411737025be063c26c29928ee37fb11b9
CHIGODI <chiegody254@gmail.com>
2024-06-28 22:25:49 +0300

    Add task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,6 +5,7 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => (
+        console.log(res)
       res.map((o) => ({
         status: o.status,
         value: o.status === 'fulfilled' ? o.value : String(o.reason),
