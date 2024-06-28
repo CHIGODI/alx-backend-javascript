6cf4da97b0511bd7cd755b188f0a7efe0c96ed4c
CHIGODI <chiegody254@gmail.com>
2024-06-28 14:45:12 +0300

    Add task 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,7 @@
 export default function createIteratorObject(report) {
   let employees = [];
   for (const department of Object.keys(report.allEmployees)) {
-    employees = employees.concat(report.allEmployees[department]);
+    employees = employees.concat(report.allEmployees.department);
   }
   return employees;
 }
