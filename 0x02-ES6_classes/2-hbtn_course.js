6ab9901d141ac76ce5f39b56c83621cc0319b7f5
CHIGODI <chiegody254@gmail.com>
2024-07-02 11:15:12 +0300

    Add task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,21 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+
+    if (Array.isArray(students) && students.every((student) => typeof student === 'string')) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
+  }
+}
