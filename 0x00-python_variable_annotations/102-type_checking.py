5886f3b007021887893c2da59097eee3dcf96005
CHIGODI <chiegody254@gmail.com>
2024-07-04 12:02:11 +0300

    Fix errors

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
 """This module contains the zoom_array function """
 
-from typing import Tuple, Iterable, TypeVar
-
-T = TypeVar('T')
+from typing import Tuple, Iterable, List
 
 
-def zoom_array(lst: Iterable[T], factor: int = 2) -> Tuple[T, ...]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """ returns tupple """
-    zoomed_in: Tuple[T, ...] = tuple(
+    zoomed_in: List = list(
         item for item in lst
         for i in range(factor)
     )
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
